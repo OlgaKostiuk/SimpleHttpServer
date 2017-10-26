3ecf3bded4d1984b5714fe1a5e74b20e04802db5
OlgaKostiuk <olga.d.kostiuk@gmail.com>
2017-10-26 16:00:43 +0300

    Add project files.

---
+++ HtmlFileServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlFileServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myFolder = @"..\..\myServerFolder";
+            //SimpleHTTPServer myServer = new SimpleHTTPServer(myFolder);
+            SimpleHTTPServer myServer = new SimpleHTTPServer(myFolder, 8084);
+            Console.WriteLine("Server is running on this port: " + myServer.Port.ToString());
+            Console.ReadLine();
+        }
+    }
+}
